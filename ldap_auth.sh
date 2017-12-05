0f715ce81bb112507326830519901f7764f24dd1
jaekwon <insainty21@gmail.com>
2017-12-05 14:11:28 +0900

    first commit

--- ldap_auth.sh
+++ ldap_auth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cn=$1
 
-ldapsearch -h ldap -D $ADMIN_DN -w $ADMIN_DN_PASS -x -b $LDAP_BASE "(&(objectclass=posixAccount)(cn=$cn))" | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
+ldapsearch -h $LDAP_SERVER -D $ADMIN_DN -w $ADMIN_DN_PASS -x -b $LDAP_BASE "(&(objectclass=posixAccount)(cn=$cn))" | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
