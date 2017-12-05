11297ab1afc77f553214df01da0bf039792dc06c
jaekwon <insainty21@gmail.com>
2017-12-05 15:23:59 +0900

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,13 @@ done
 
 
 # ssh public key access config
-if [[ $(grep "AuthorizedKeysCommand " /etc/ssh_sshd_config | wc -l) -eq 0]]; then
+if [[ $(grep -c "AuthorizedKeysCommand " /etc/ssh_sshd_config ) -eq 0 ]]; then   
   echo "AuthorizedKeysCommand /usr/local/bin/ldap_auth.sh" >> /etc/ssh/sshd_config
 fi
-if [[ $(grep "AuthorizedKeysCommandUser " /etc/ssh_sshd_config | wc -l) -eq 0]]; then
+if [[ $(grep -c "AuthorizedKeysCommandUser " /etc/ssh_sshd_config ) -eq 0 ]]; then
   echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
 fi
-if [[ $(grep "AuthorizedKeysFile /dev/null" /etc/ssh_sshd_config | wc -l) -eq 0]]; then
+if [[ $(grep -c "AuthorizedKeysFile /dev/null" /etc/ssh_sshd_config ) -eq 0 ]]; then
   echo "AuthorizedKeysFile /dev/null" >> /etc/ssh/sshd_config
 fi
 
